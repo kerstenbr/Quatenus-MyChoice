09dbae9ada4f289bcef19874a261e9515ec8c4f7
Guilherme Kersten da Silva <kersten.kdasilva@gmail.com>
2024-01-25 16:32:48 -0300

    FIX: Excluido todos os comentários sobre a antiga versão e arrumado alguns espaços

--- js/searchbar.js
+++ js/searchbar.js
@@ -4,11 +4,6 @@ const searchInput = document.querySelector("[data-search]");
 
 let products = [];
 
-// ------ Todos os rastreadores devem ser cadastrados aqui na ordem de visualização deles dentro do modal! ------
-// const orderOfFields = [];
-// const orderOfFields = ["gl200", "gl300", "gl320mg", "gl500", "gv55", "gv65", "gv300", "gv300n", "gv300can", "gv350mg", "gv355 ceu"];
-// ------ Todos os rastreadores devem ser cadastrados aqui na ordem de visualização deles dentro do modal! ------
-
 // Função para carregar os produtos a partir do arquivo json
 function loadProductsFromJson() {
     fetch("/familias.json")
@@ -83,25 +78,6 @@ function createProductModal(product) {
         modalElement = document.createElement('div');
         modalElement.id = modalId;
         modalElement.classList.add('modal', 'fade');
-
-        // const gaModalButton = orderOfFields.map(gaKey => {
-        //     const gaBtnSrc = product.gaModal[gaKey];
-        //     return gaBtnSrc ? `
-        //         <div class="d-inline-block">
-        //             <button class="btn btn-qblue mb-3" id="btn${gaKey}" data-bs-toggle="collapse" data-bs-target="#collapse${gaKey}" aria-expanded="false" aria-controls="collapse${gaKey}">
-        //                 ${gaKey.toLocaleUpperCase()}
-        //             </button>
-        //         </div>` : '';
-        // }).join('');
-
-        // const gaModalContentImg = orderOfFields.map(gaKey => {
-        //     const gaImgSrc = product.gaModal[gaKey];
-        //     return gaImgSrc ? `
-        //         <div class="collapse card shadow-sm mb-2" id="collapse${gaKey}">
-        //             <h6 class="mt-1 ms-2">${gaKey.toLocaleUpperCase()}</h6>
-        //             <img class="img-fluid" src="${gaImgSrc}" alt="${gaKey.toLocaleUpperCase()}">
-        //         </div>` : '';
-        // }).join('');
         
         const productsSegments = Object.entries(product.prodSimModal).map(([similarProduct, price]) => {
             console.log("Produtos similares: " + similarProduct) // ${similarProduct.toLocaleUpperCase()} ${price[0]}
@@ -120,8 +96,6 @@ function createProductModal(product) {
                 </tbody>
                 ` : '';
         }).join('');
-
-        
 
         modalElement.innerHTML = `
             <div class="modal-dialog modal-xl">
@@ -180,30 +154,9 @@ function createProductModal(product) {
     modal.show();
 
     const closeHandler = () => {
-        // document.body.removeEventListener('click', buttonClickHandler);
         document.removeEventListener('hidden.bs.modal', closeHandler);
     };
 
-    // FIXME: Se a clicar muito rápido a cor do botão não muda
-    // const buttonClickHandler = (event) => {
-    //     const targetId = event.target.id;
-    //     // if (targetId && targetId.startsWith('btn')) {
-    //     if (targetId.startsWith('btn')) {
-    //         const btnElement = document.getElementById(targetId);
-
-    //         if (btnElement.classList.contains("btn-qblue")) {
-    //             // console.log("Botão clicado pela primeira vez");
-    //             btnElement.classList.remove("btn-qblue");
-    //             btnElement.classList.add("btn-qorange");
-    //         } else if (btnElement.classList.contains("btn-qorange")) {
-    //             // console.log("Botão clicado pela segunda vez");
-    //             btnElement.classList.remove("btn-qorange");
-    //             btnElement.classList.add("btn-qblue");
-    //         }
-    //     }
-    // };
-
-    // document.body.addEventListener('click', buttonClickHandler);
     document.addEventListener('hidden.bs.modal', closeHandler);
 
     modalElement.addEventListener('hidden.bs.modal', () => {
